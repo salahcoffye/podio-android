7ee5a93cc16a1237477b49659b586fa5a0e7815d
Salah Abdul Salam <salah.coffye@live.com>
2017-07-08 03:06:20 +0530

    bug fix

--- sdk/src/main/java/com/podio/sdk/androidasynchttp/AndroidAsyncHttpClient.java
+++ sdk/src/main/java/com/podio/sdk/androidasynchttp/AndroidAsyncHttpClient.java
@@ -44,11 +44,10 @@ public class AndroidAsyncHttpClient implements Client {
 
     @Override
     public <T> Request<T> request(Request.Method method, Filter filter, Object item, Class<T> classOfResult) {
-        if (filter instanceof FileProvider.FileFilter && method == Request.Method.POST && item instanceof File) {
+        if (filter instanceof FileProvider.FileFilter && method == Request.Method.POST) {
             String url = filter.buildUri(scheme, authority).toString();
             AndroidAsyncHttpRequest<T> request = new AndroidAsyncHttpRequest<>(client, context, url, (File) item, classOfResult);
             request.performRequest();
-
             return request;
         } else {
             throw new UnsupportedOperationException("AndroidAsyncHttpClient only supports uploading to our Files API.");
